16f219784620e73cc76443b4e503d57128b362f9
Shakil57375 <shakil57375@gmail.com>
2024-03-04 14:53:10 +0600

    auth token and refresh token added

--- src/Components/auth/LoginForm.jsx
+++ src/Components/auth/LoginForm.jsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import Field from "../common/Field";
 import { useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth/useAuth";
+import axios from "axios";
 
 const LoginForm = () => {
   const navigate = useNavigate();
@@ -10,11 +11,31 @@ const LoginForm = () => {
     register,
     handleSubmit,
     formState: { errors },
+    setError,
   } = useForm();
-  const submitForm = (formData) => {
-    const user = { ...formData };
-    setAuth({ user });
-    navigate("/");
+  const submitForm = async (formData) => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_SERVER_BASE_URL}/auth/login`,
+        formData
+      );
+      if (response.status === 200) {
+        const { token, user } = response.data;
+        if (token) {
+          const authToken = token.token;
+          const refreshToken = token.refreshToken;
+          console.log(`Login time auth token : ${authToken}`);
+          setAuth({ user, authToken, refreshToken });
+          navigate("/");
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      setError("root.random", {
+        type: "random",
+        message: `User with email ${formData.email} is not found`,
+      });
+    }
   };
   return (
     <form
@@ -49,6 +70,7 @@ const LoginForm = () => {
           id="password"
         />
       </Field>
+      <p>{errors?.root?.random?.message}</p>
       <Field>
         <button
           className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
