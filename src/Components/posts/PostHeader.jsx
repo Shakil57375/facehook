4191c5ceb89ddfc71624ca8161fb4cc689dbe591
Shakil57375 <shakil57375@gmail.com>
2024-03-05 22:37:57 +0600

    days horus and miniutes calculated

--- src/Components/posts/PostHeader.jsx
+++ src/Components/posts/PostHeader.jsx
@@ -2,12 +2,45 @@ import ThreeDotsIcon from "../../assets/icons/3dots.svg";
 import EditIcons from "../../assets/icons/edit.svg";
 import DeleteIcons from "../../assets/icons/delete.svg";
 import TimeIcon from "../../assets/icons/time.svg";
-const PostHeader = ({post}) => {
-  return (
-    <div>
-      <p>header</p>
-    </div>
-  )
-}
+import { getDateDifferenceFromNow } from "../../utils";
+const PostHeader = ({ post }) => {
+    return (
+        <header className="flex items-center justify-between gap-4">
+            <div className="flex items-center gap-3">
+                <img
+                    className="max-w-10 max-h-10 rounded-full lg:max-h-[58px] lg:max-w-[58px]"
+                    src="./assets/images/avatars/avatar_1.png"
+                    alt="avatar"
+                />
+                <div>
+                    <h6 className="text-lg lg:text-xl">{post?.author?.name}</h6>
+                    <div className="flex items-center gap-1.5">
+                        <img src={TimeIcon} alt="time" />
+                        <span className="text-sm text-gray-400 lg:text-base">
+                           {`${getDateDifferenceFromNow(post?.createAt)} ago`}
+                        </span>
+                    </div>
+                </div>
+            </div>
 
-export default PostHeader
+            <div className="relative">
+                <button>
+                    <img src={ThreeDotsIcon} alt="3dots of Action" />
+                </button>
+
+                <div className="action-modal-container">
+                    <button className="action-menu-item hover:text-lwsGreen">
+                        <img src={EditIcons} alt="Edit" />
+                        Edit
+                    </button>
+                    <button className="action-menu-item hover:text-red-500">
+                        <img src={DeleteIcons} alt="Delete" />
+                        Delete
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default PostHeader;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,24 +1,45 @@
-export const getDateDifferenceFromNow = (formDate) => {
-    let difference = new Date().getTime() - new Date(formDate).getTime();
+export const getDateDifferenceFromNow = (fromDate) => {
+    let difference = new Date().getTime() - new Date(fromDate).getTime();
 
-    difference = difference / 1000;
-    let hourDifference = Math.floor(difference / 3600);
-    difference = difference - hourDifference * 3600;
-    let minuteDifference = Math.floor(difference / 60);
-    difference = difference - minuteDifference * 60;
-    let message;
-    if (hourDifference > 0) {
-        message = `${hourDifference} hour`;
+    difference = difference / (1000 * 3600 * 24); // Convert milliseconds to days
+    let dayDifference = Math.floor(difference); // Get the whole number of days
+    difference = difference - dayDifference; // Remove the days from the difference
+
+    let hourDifference = Math.floor(difference * 24);  // Convert days back to hours
+    difference = difference * 24 - hourDifference;  // Remove the hours from the difference
+
+    let minuteDifference = Math.floor(difference * 60);  // Calculate the whole number of minutes
+    difference = difference * 60 - minuteDifference; // Remove the minutes from the difference
+
+
+    let message = '';
+
+    if (dayDifference > 0) {
+        message = `${dayDifference} day`;
+        if (dayDifference > 1) {
+            message += 's';
+        }
     }
+
+    if (hourDifference > 0) {
+        message = message
+            ? `${message} ${hourDifference} hour`
+            : `${hourDifference} hour`;
+        if (hourDifference > 1) {
+            message += 's';
+        }
+    }
+
     if (minuteDifference > 0) {
         message = message
-            ? `${message} ${minuteDifference} minutes`
-            : `${minuteDifference} minutes`;
+            ? `${message} ${minuteDifference} minute`
+            : `${minuteDifference} minute`;
+        if (minuteDifference > 1) {
+            message += 's';
+        }
     }
-    if (difference) {
-        message = message
-            ? `${message} ${Math.round(difference)} minutes`
-            : `${Math.round(difference)} minutes`;
-    }
+
+   
+
     return message;
 };
