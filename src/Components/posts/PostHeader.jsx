e9d8b1a8c9a33a08c013cfa49c638cb83e2593f6
Shakil57375 <shakil57375@gmail.com>
2024-03-05 22:56:46 +0600

    post action toggle added

--- src/Components/posts/PostHeader.jsx
+++ src/Components/posts/PostHeader.jsx
@@ -4,8 +4,10 @@ import DeleteIcons from "../../assets/icons/delete.svg";
 import TimeIcon from "../../assets/icons/time.svg";
 import { getDateDifferenceFromNow } from "../../utils";
 import useAvatar from "../../hooks/useAvatar/useAvatar";
+import { useState } from "react";
 const PostHeader = ({ post }) => {
-  const {avatarURL} = useAvatar(post)
+    const { avatarURL } = useAvatar(post);
+    const [showAction, setShowAction] = useState(false);
     return (
         <header className="flex items-center justify-between gap-4">
             <div className="flex items-center gap-3">
@@ -19,27 +21,29 @@ const PostHeader = ({ post }) => {
                     <div className="flex items-center gap-1.5">
                         <img src={TimeIcon} alt="time" />
                         <span className="text-sm text-gray-400 lg:text-base">
-                           {`${getDateDifferenceFromNow(post?.createAt)} ago`}
+                            {`${getDateDifferenceFromNow(post?.createAt)} ago`}
                         </span>
                     </div>
                 </div>
             </div>
 
             <div className="relative">
-                <button>
+                <button onClick={() => setShowAction(!showAction)}>
                     <img src={ThreeDotsIcon} alt="3dots of Action" />
                 </button>
 
-                <div className="action-modal-container">
-                    <button className="action-menu-item hover:text-lwsGreen">
-                        <img src={EditIcons} alt="Edit" />
-                        Edit
-                    </button>
-                    <button className="action-menu-item hover:text-red-500">
-                        <img src={DeleteIcons} alt="Delete" />
-                        Delete
-                    </button>
-                </div>
+                {showAction && (
+                    <div className="action-modal-container">
+                        <button className="action-menu-item hover:text-lwsGreen">
+                            <img src={EditIcons} alt="Edit" />
+                            Edit
+                        </button>
+                        <button className="action-menu-item hover:text-red-500">
+                            <img src={DeleteIcons} alt="Delete" />
+                            Delete
+                        </button>
+                    </div>
+                )}
             </div>
         </header>
     );
