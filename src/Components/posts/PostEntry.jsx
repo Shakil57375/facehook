074e0e94a39c7cd70ae75a5949ef85fc8b4f6f36
Shakil57375 <shakil57375@gmail.com>
2024-03-06 17:27:18 +0600

    template added

--- src/Components/posts/PostEntry.jsx
+++ src/Components/posts/PostEntry.jsx
@@ -1,14 +1,17 @@
 import { useForm } from "react-hook-form";
 import useAuth from "../../hooks/useAuth/useAuth";
 import useAxios from "../../hooks/useAxios/useAxios";
+import AddPhoto from "../../assets/icons/addPhoto.svg"
 import { usePost } from "../../hooks/usePost/usePost";
 import useProfile from "../../hooks/useProfile/useProfile";
+import Field from "../common/Field";
 
 const PostEntry = ({ onCreate }) => {
     const { auth } = useAuth();
     const { dispatch } = usePost();
     const { api } = useAxios();
     const { state: profile } = useProfile();
+    const user = profile?.user ?? auth?.user
     const {
         register,
         handleSubmit,
@@ -26,7 +29,59 @@ const PostEntry = ({ onCreate }) => {
                 Create Post
             </h6>
             <form onSubmit={handleSubmit(handlePostSubmit)}>
+                <div className="mb-3 flex items-center justify-between gap-2 lg:mb-6 lg:gap-4">
+                    <div className="flex items-center gap-3">
+                        <img
+                            className="max-w-10 max-h-10 rounded-full lg:max-h-[58px] lg:max-w-[58px]"
+                            src={`${import.meta.env.VITE_SERVER_BASE_URL}/${
+                                user?.avatar
+                            }`}
+                            alt="avatar"
+                        />
+                        <div>
+                            <h6 className="text-lg lg:text-xl">
+                                {user?.firstName} {user?.lastName}{" "}
+                            </h6>
 
+                            <span className="text-sm text-gray-400 lg:text-base">
+                                Public
+                            </span>
+                        </div>
+                    </div>
+
+                    <label
+                        className="btn-primary cursor-pointer !text-gray-100"
+                        htmlFor="photo"
+                    >
+                        <img src={AddPhoto} alt="Add Photo" />
+                        Add Photo
+                    </label>
+                    <input
+                        type="file"
+                        name="photo"
+                        id="photo"
+                        className="hidden"
+                    />
+                </div>
+                <Field label="" error={errors.content}>
+                    <textarea
+                        {...register("content", {
+                            required: "Adding some text is mandatory!",
+                        })}
+                        name="content"
+                        id="content"
+                        placeholder="Share your thoughts..."
+                        className="h-[120px] w-full bg-transparent focus:outline-none lg:h-[160px]"
+                    ></textarea>
+                </Field>
+                <div className="border-t border-[#3F3F3F] pt-4 lg:pt-6">
+                    <button
+                        className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
+                        type="submit"
+                    >
+                        Post
+                    </button>
+                </div>
             </form>
         </div>
     );
