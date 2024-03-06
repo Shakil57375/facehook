99b2af832130a035724a0d997b6f440135d8a465
Shakil57375 <shakil57375@gmail.com>
2024-03-06 17:09:23 +0600

    sort functionalites added based on createdAt

--- src/Components/posts/PostEntry.jsx
+++ src/Components/posts/PostEntry.jsx
@@ -1,9 +1,35 @@
-const PostEntry = ({onCreate}) => {
-  return (
-    <div>
-      <p>Post entry</p>
-    </div>
-  )
-}
+import { useForm } from "react-hook-form";
+import useAuth from "../../hooks/useAuth/useAuth";
+import useAxios from "../../hooks/useAxios/useAxios";
+import { usePost } from "../../hooks/usePost/usePost";
+import useProfile from "../../hooks/useProfile/useProfile";
 
-export default PostEntry
+const PostEntry = ({ onCreate }) => {
+    const { auth } = useAuth();
+    const { dispatch } = usePost();
+    const { api } = useAxios();
+    const { state: profile } = useProfile();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        setError,
+    } = useForm();
+
+    const handlePostSubmit = (formData) =>{
+        console.log(formData)
+    }
+
+    return (
+        <div className="card relative">
+            <h6 className="mb-3 text-center text-lg font-bold lg:text-xl">
+                Create Post
+            </h6>
+            <form onSubmit={handleSubmit(handlePostSubmit)}>
+
+            </form>
+        </div>
+    );
+};
+
+export default PostEntry;
